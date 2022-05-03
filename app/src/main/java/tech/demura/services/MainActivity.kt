01179366d9a8abe668df76602d234150ae05adf5
Roman Demura <anokhin.roman.27@gmail.com>
2022-05-03 10:09:26 +0300

    Initial commit

---
+++ app/src/main/java/tech/demura/services/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.demura.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
