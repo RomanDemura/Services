0e770ae971a067dc875c864f4ba06307f901dd61
Roman Demura <anokhin.roman.27@gmail.com>
2022-05-03 12:37:00 +0300

    Notifications

--- app/src/main/java/tech/demura/services/MainActivity.kt
+++ app/src/main/java/tech/demura/services/MainActivity.kt
@@ -1,7 +1,12 @@
 package tech.demura.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import tech.demura.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +23,34 @@ class MainActivity : AppCompatActivity() {
             startService(SimpleService.newIntent(this, 25))
         }
 
+        binding.btnForegroundService.setOnClickListener {
+            showNotification()
+        }
+
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(com.google.android.material.R.drawable.ic_clock_black_24dp)
+            .build()
+
+
+        notificationManager.notify(1, notification)
+    }
+
+    companion object{
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
